655898da17d601b50c32bd2e1f9471a6c860a847
QiuRan <yqr666989@163.com>
2026-09-25 21:45:52 +0800

    添加合并脚本、上传说明和源码包

---
+++ 发行包/合并分卷.bat
@@ -0,0 +1,7 @@
+@echo off
+chcp 65001 >nul
+cd /d %~dp0
+echo 正在合并 4 个分卷 ...
+copy /b 抖音续火花_成品版_打开即用.zip.part01+抖音续火花_成品版_打开即用.zip.part02+抖音续火花_成品版_打开即用.zip.part03+抖音续火花_成品版_打开即用.zip.part04 "抖音续火花_成品版_打开即用.zip" >nul
+echo 合并完成：抖音续火花_成品版_打开即用.zip，解压即可使用。
+pause
